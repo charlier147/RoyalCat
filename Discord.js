33202858455069854774daa1bc29332e4d6eacb1
charlier147 <30325217+charlier147@users.noreply.github.com>
2018-02-14 14:33:32 -0800

    Create Discord.js

---
+++ Discord.js
@@ -0,0 +1,26 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+client.on("ready", () => {
+  console.log("im readyyyyy");
+  
+  client.user.setPresence({ game: { name: "raidcentral.com", type: 0 } });
+});
+
+var prefix = "!"
+
+client.on('message', msg => {
+  if (msg.author.bot) return;
+  if (msg.content.indexOf(prefix) !== 0) return;
+
+  const args = msg.content.slice(prefix.length).trim().split(/ +/g);
+  const command = args.shift().toLowerCase()
+
+  if (command === "ping") {
+    msg.delete();
+    msg.channel.send("Pong...").then((msg) => {
+      msg.edit(`Pong! Latency is ${msg.createdTimestamp - msg.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
+    });
+  }
+
+client.login(process.env.BOT_TOKEN);
