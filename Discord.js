433a45500ce0cdef9763d8296e8bdd6fee67be2c
charlier147 <30325217+charlier147@users.noreply.github.com>
2018-02-14 19:33:56 -0800

    Update Discord.js

--- Discord.js
+++ Discord.js
@@ -3,11 +3,12 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
   console.log('HFC -  STARTING!');
-});
-client.on('message', message => {
-  if (message.content === 'what is my avatar') {
-    message.reply(message.author.avatarURL);
-  }
+
+  });
+  client.on('guildMemberAdd', member => {
+  const channel = member.guild.channels.find('name', 'member-log');
+  if (!channel) return;
+  channel.send(`Welcome to HFC CLAN ${member}! :dizzy~1:. Please read #rules-information!`);
 });
 
 client.login(process.env.TOKEN);
