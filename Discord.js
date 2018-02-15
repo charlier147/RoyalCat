70bb071c9ad91acd6ed646df6cfefdb91cb7764b
charlier147 <30325217+charlier147@users.noreply.github.com>
2018-02-15 15:54:39 -0800

    Update Discord.js

--- Discord.js
+++ Discord.js
@@ -29,6 +29,10 @@ client.on('message', message => {
 		}
 		client.user.setStatus(result);
 	}
+	if (message.content.startsWith(prefix + 'ping')) {
+        const m = await message.channel.send("Ping?");
+        m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
+  }
 	
 });
 
