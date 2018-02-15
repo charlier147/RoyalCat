12042d2f0990c0c30adca63a560e552762431b0b
charlier147 <30325217+charlier147@users.noreply.github.com>
2018-02-14 19:37:56 -0800

    Update Discord.js

--- Discord.js
+++ Discord.js
@@ -4,11 +4,23 @@ const client = new Discord.Client();
 client.on('ready', () => {
   console.log('HFC -  STARTING!');
 
-  });
-  client.on('guildMemberAdd', member => {
-  const channel = member.guild.channels.find('name', 'member-log');
-  if (!channel) return;
-  channel.send(`Welcome to HFC CLAN ${member}! :dizzy~1:. Please read #rules-information!`);
+ });
+
+client.on("guildMemberAdd", (member) => {
+  const guild = member.guild;
+  if (!newUsers[guild.id]) newUsers[guild.id] = new Discord.Collection();
+  newUsers[guild.id].set(member.id, member.user);
+
+  if (newUsers[guild.id].size > 10) {
+    const userlist = newUsers[guild.id].map(u => u.toString()).join(" ");
+    guild.channels.get(guild.id).send("Welcome \n" + userlist + "! Make sure to read #rules-information!);
+    newUsers[guild.id].clear();
+  }
+});
+
+client.on("guildMemberRemove", (member) => {
+  const guild = member.guild;
+  if (newUsers[guild.id].has(member.id)) newUsers.delete(member.id);
 });
 
 client.login(process.env.TOKEN);
