bd8accf4de74c4b2bcebb5a66c2459a1c38ba867
charlier147 <30325217+charlier147@users.noreply.github.com>
2018-02-14 19:27:40 -0800

    Update Discord.js

--- Discord.js
+++ Discord.js
@@ -4,13 +4,10 @@ const client = new Discord.Client();
 client.on('ready', () => {
   console.log('HFC -  STARTING!');
 });
-
-client.on('message', msg => {
-    if (!msg.content.startsWith(process.env.PREFIX) || !msg.guild) return;
-    const command = msg.content.split(' ')[0].substr(process.env.PREFIX.length);
-    const args = msg.content.split(' ').slice(1).join(' ');
-    if (command === 'guide') return msg.channel.send('');
-    else if (command === 'invite') return msg.channel.send(process.env.INVITE);
+client.on('message', message => {
+  if (message.content === 'what is my avatar') {
+    message.reply(message.author.avatarURL);
+  }
 });
 
 client.login(process.env.TOKEN);
