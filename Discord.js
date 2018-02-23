2be7ccad5deb576c6a954300b42a7f4aa27e8174
charlier147 <30325217+charlier147@users.noreply.github.com>
2018-02-22 19:36:18 -0800

    Update Discord.js

--- Discord.js
+++ Discord.js
@@ -7,20 +7,11 @@ client.on("ready", () => {
   client.user.setPresence({ game: { name: "Overwatch", type: 0 } });
 });
 
-var prefix = "++"
-
-client.on('message', msg => {
-  if (msg.author.bot) return;
-  if (msg.content.indexOf(prefix) !== 0) return;
-
-  const args = msg.content.slice(prefix.length).trim().split(/ +/g);
-  const command = args.shift().toLowerCase()
-
-  if (command === "ping") {
-    msg.delete();
-    msg.channel.send("Pong...").then((msg) => {
-      msg.edit(`Pong! Latency is ${msg.createdTimestamp - msg.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
-    });
+if(command === "say") {
+    const sayMessage = args.join(" ");
+    message.delete().catch(O_o=>{}); 
+    message.channel.send(sayMessage);
+  }};
   }
 
 client.login(process.env.TOKEN);
