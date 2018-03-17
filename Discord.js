7720b0ade920609f8cbd9c4c0db5365641ddfc19
charlier147 <30325217+charlier147@users.noreply.github.com>
2018-03-17 15:10:28 -0700

    Update Discord.js

--- Discord.js
+++ Discord.js
@@ -27,9 +27,7 @@ client.on('message', msg => {
 
   if (command === "ping") {
     msg.channel.send("Pong...").then((msg) => {
-      msg.edit(`Pong! Latency is ${msg.createdTimestamp - msg.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`)
-    .then(msg => {
-    msg.delete(10000)
+      msg.edit(`Pong! Latency is ${msg.createdTimestamp - msg.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
     });
   }
 
