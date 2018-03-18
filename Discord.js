dc2a9baa479a6f4208a21f13698646652c908f20
charlier147 <30325217+charlier147@users.noreply.github.com>
2018-03-18 10:38:42 -0700

    Update Discord.js

--- Discord.js
+++ Discord.js
@@ -1,6 +1,26 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
+bot.commands = new Discord.Collection();
+
+fs.readdir("./commands/", (err, files) => {
+
+  if(err) console.log(err);
+
+  let jsfile = files.filter(f => f.split(".").pop() === "js")
+  if(jsfile.length <= 0){
+    console.log("Couldn't find commands.");
+    return;
+  }
+
+  jsfile.forEach((f, i) =>{
+    let props = require(`./commands/${f}`);
+    console.log(`${f} loaded!`);
+    bot.commands.set(props.help.name, props);
+  });
+
+});
+
 client.on("ready", () => {
   console.log("im readyyyyy");
      client.user.setStatus("dnd");
