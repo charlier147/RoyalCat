b7992bf171f7e14cd75e6e02fa89f8d8e16985a2
charlier147 <30325217+charlier147@users.noreply.github.com>
2018-03-20 13:33:25 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,41 +1,7 @@
-const botconfig = require("./config.json");
-const tokenfile = require("./token.json");
 const Discord = require("discord.js");
-const fs = require("fs");
-const bot = new Discord.Client({disableEveryone: true});
-bot.commands = new Discord.Collection();
+const bot = new Discord.Client();
+const Config = require("./config.json");
 
-fs.readdir("./commands/", (err, files) => {
+require("./eventloader/loader.js").run(bot);
 
-  if(err) console.log(err);
-  let jsfile = files.filter(f => f.split(".").pop() === "js")
-  if(jsfile.length <= 0){
-    console.log("Couldn't find commands.");
-    return;
-  }
-
-  jsfile.forEach((f, i) =>{
-    let props = require(`./commands/${f}`);
-    console.log(`${f} loaded!`);
-    bot.commands.set(props.help.name, props);
-  });
-});
-
-bot.on("ready", async () => {
-  console.log(`${bot.user.username} is online on ${bot.guilds.size} servers!`);
-
-});
-
-bot.on("message", async message => {
-  if(message.author.bot) return;
-  if(message.channel.type === "dm") return;
-
-  let prefix = botconfig.prefix;
-  let messageArray = message.content.split(" ");
-  let cmd = messageArray[0];
-  let args = messageArray.slice(1);
-  let commandfile = bot.commands.get(cmd.slice(prefix.length));
-  if(commandfile) commandfile.run(bot,message,args);
-
-});
 bot.login(process.env.TOKEN);
