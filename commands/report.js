8d15f70f9d8ca369da21e5c1c38a038921ad17fe
charlier147 <30325217+charlier147@users.noreply.github.com>
2018-03-21 12:17:28 -0700

    Update report.js

--- commands/report.js
+++ commands/report.js
@@ -6,6 +6,8 @@ module.exports.run = async (bot, message, args) => {
   color: 3447003,
   description: "Couldn't find the player."      
 }});
+    .then(message => {
+    message.delete(10000)
     let rreason = args.join(" ").slice(22);
 
     let reportEmbed = new Discord.RichEmbed()
