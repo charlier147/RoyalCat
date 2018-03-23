86cd281c5535e85be6dd6621d736611483657ebc
charlier147 <30325217+charlier147@users.noreply.github.com>
2018-03-23 16:59:40 -0700

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -5,12 +5,11 @@ module.exports.run = async (client, message, args) => {
         var lat_ms = (m.createdTimestamp - message.createdTimestamp);
         var api_ms = (Math.round(client.ping));
   
-        m.delete().catch(console.error);
   
         const embed = new Discord.MessageEmbed()
             .setColor('#16CDC7')
             .addField(`📥     \`Latency\``, `       \`${lat_ms}ms\``, true)
             .addField(`📤     \`API\``, `       \`${api_ms}ms\``, true)
         message.channel.send(embed);
-    }).catch(console.error);
+    });
 } 
